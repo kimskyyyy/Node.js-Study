436cfd3f35c12e2b9d9ea0be10a831427551c7fb
kimskyyyy <hhhhh823@naver.com>
2023-04-27 23:04:54 +0900

    main.js 24-4 예제

--- main.js
+++ main.js
@@ -11,82 +11,75 @@ var app = http.createServer(function(request,response){
 
     if(pathname === '/') { // 루트라면 기존 코드 실행
       if(queryData.id === undefined) { // 홈 인 경우
-        
+        // 파일 목록 가져오기
+        fs.readdir('./data', function(error, filelist){
+          console.log(filelist);
           var title = 'Welcom'; // 제목 입력
           var description = 'Hello , Node.js'; // 내용 입력
+          var list = '<ul>'; // list 변수 선언
+          var i = 0; // 반복 횟수를 위한 변수 i 선언하고 0으로 초기화
+          while(i < filelist.length) { // filelist에 담긴 원소 개수만큼 반복
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>'; // list 변수를 이용해서 <ul></ul> 태그 만들기
           var template = `
           <!doctype html>
           <html>
-          <head>
-            <title>WEB - ${title}</title>
-            <meta charset="utf-8">
-            <link rel="stylesheet" href="style.css">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <div id="grid">
-              <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ul>
-              <div id="article">
-                <h2>${title}</h2>
-                <p>${description}</p>
-                <p>
-                  Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript it forms a triad of cornerstone technologies for the World Wide Web.[2] Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-                </p>
-              </div>
-            </div>
+            <head>
+              <title>WEB - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
             </body>
-            </html>
+          </html>
           `;
           response.writeHead(200);
           response.end(template);
+        });
 
       } else { // 홈이 아닌 경우
+         // 파일 목록 가져오기
+         fs.readdir('./data', function(error, filelist){
+          console.log(filelist);
+          var list = '<ul>'; // list 변수 선언
+          var i = 0; // 반복 횟수를 위한 변수 i 선언하고 0으로 초기화
+          while(i < filelist.length) { // filelist에 담긴 원소 개수만큼 반복
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '</ul>'; // list 변수를 이용해서 <ul></ul> 태그 만들기
+
         fs.readFile(`data/${queryData.id}`,'utf8',function(err, description) { // 파일의 내용을 description 변수에 저장
           var title = queryData.id; // 쿼리 스트링에서 제목을 가져옴
           var template = `
           <!doctype html>
           <html>
-          <head>
-            <title>WEB - ${title}</title>
-            <meta charset="utf-8">
-            <link rel="stylesheet" href="style.css">
-          </head>
-          <body>
+            <head>
+              <title>WEB - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
             <h1><a href="/">WEB</a></h1>
-            <div id="grid">
-              <ul>
-                <li><a href="/?id=HTML">HTML</a></li>
-                <li><a href="/?id=CSS">CSS</a></li>
-                <li><a href="/?id=JavaScript">JavaScript</a></li>
-              </ul>
-              <div id="article">
-                <h2>${title}</h2>
-                <p>${description}</p>
-                <p>
-                  Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript it forms a triad of cornerstone technologies for the World Wide Web.[2] Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document.
-                </p>
-              </div>
-            </div>
-            </body>
-            </html>
+            ${list}
+            <h2>${title}</h2>
+            <p>${description}</p>
+              </body>
+          </html>
           `;
           response.writeHead(200);
           response.end(template);
         });
+      });
       }
-      
     } else { // 루트가 아니라면 새로운 코드 실행
       response.writeHead(404);
       response.end('Not found');
     }
-
-   
-    
-   
  
 });
 
