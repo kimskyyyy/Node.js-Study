a1e18629a974f63fbb200e499a6188d3c2e13b38
kimskyyyy <hhhhh823@naver.com>
2023-04-30 23:37:28 +0900

    body-parser 적용

--- main.js
+++ main.js
@@ -5,7 +5,20 @@ var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
+var bodyParser = require('body-parser');
+var compression = require('compression');
 
+
+// body-parser 문서 따라하기, express use메서드에 body-parser 미들웨어 전달하기
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(compression());
+// 미들웨어 만들기
+app.get('*', function(request, response, next) {
+  fs.readdir('./data', function(error, filelist) {
+    request.list = filelist;
+    next();
+  });
+});
 
 
 // get 메서드의 첫번째 인자는 경로, 두번째 인자는 콜백함수가 들어감
@@ -13,49 +26,44 @@ var qs = require('querystring');
 // app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/', function(request, response) {
-  fs.readdir('./data', function(error, filelist){ // 파일 목록 가져오기
-    var title = 'Welcom'; // 제목 입력
-    var description = 'Hello , Node.js'; // 내용 입력
-    var list = template.list(filelist);
-    var html = template.HTML(title, list,
-       `<h2>${title}</h2>${description}`
-       );
-       response.send(html)
-  });
+  var title = 'Welcom'; // 제목 입력
+  var description = 'Hello , Node.js'; // 내용 입력
+  var list = template.list(request.list);
+  var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`
+      );
+      response.send(html)
 });
 
 
 // 경로를 매개변수로 받아서 응답해주는 방식
 app.get('/page/:pageId', function(request, response) {
-  fs.readdir('./data', function(error, filelist){ // 파일 목록 가져오기
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`,'utf8',function(err, description) { // 파일의 내용을 description 변수에 저장
-      var title = request.params.pageId; // 쿼리 스트링에서 제목을 가져옴
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags:['h1']
-      });
-      var list = template.list(filelist);
-      var html = template.HTML(sanitizedTitle, list,
-         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-         `<a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-          <input type="hidden" name="id" value="${sanitizedTitle}">
-          <input type="submit" value="delete">
-          </form>
-         `
-         );
-      response.send(html);
-      });
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`,'utf8',function(err, description) { // 파일의 내용을 description 변수에 저장
+    var title = request.params.pageId; // 쿼리 스트링에서 제목을 가져옴
+    var sanitizedTitle = sanitizeHtml(title);
+    var sanitizedDescription = sanitizeHtml(description, {
+      allowedTags:['h1']
+    });
+    var list = template.list(request.list);
+    var html = template.HTML(sanitizedTitle, list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        `<a href="/create">create</a>
+        <a href="/update/${sanitizedTitle}">update</a>
+        <form action="/delete_process" method="post">
+        <input type="hidden" name="id" value="${sanitizedTitle}">
+        <input type="submit" value="delete">
+        </form>
+        `
+        );
+    response.send(html);
     });
 });
 
 // 새 글 작성(create)
 app.get('/create', function(request, response) {
-  fs.readdir('./data', function(error, filelist) {
     var title = 'WEB - create';
-    var list = template.list(filelist);
+    var list = template.list(request.list);
     var html = template.HTML(title, list, `
     <form action="/create_process" method ="post">
     <p><input type="text" name="title" placeholder="title"></p>
@@ -68,71 +76,53 @@ app.get('/create', function(request, response) {
     </form>
     `, '');
     response.send(html);
-  })
-})
+});
 
 // 새 글 작성 처리(create process)
 app.post('/create_process', function(request, response) {
-  var body = '';
-  request.on('data', function(data) {
-    body = body + data;
-  });
-  request.on('end', function() {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-      response.writeHead(302, {Location:`/?=${title}`});
-      response.end();
-    });
+  var post = request.body;  
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+    response.writeHead(302, {Location:`/?=${title}`});
+    response.end();
   });
 });
 
 // 글 수정(update)
 app.get('/update/:pageId', function(request, response) {
-  fs.readdir('./data', function(error, filelist) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
-      var title = request.params.pageId;
-      var list = template.list(filelist);
-      var html = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-        <input type="hidden" name="id" value=${title}>
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update/${title}">update</a>`
-        );
-        response.send(html);
-    });
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description) {
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(title, list,
+      `
+      <form action="/update_process" method="post">
+      <input type="hidden" name="id" value=${title}>
+      <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+      <p>
+        <textarea name="description" placeholder="description">${description}</textarea>
+      </p>
+      <p>
+        <input type="submit">
+      </p>
+      </form>
+      `,
+      `<a href="/create">create</a> <a href="/update/${title}">update</a>`
+      );
+      response.send(html);
   });
 });
 
 // 글 수정 처리(update process)
 app.post('/update_process', function(request, response) {
-  var body = '';
-  request.on('data', function(data) {
-    body = body + data;
-  });
-  request.on('end', function() {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function(error) {
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
-      response.writeHead(302, {Location:`/?=${title}`});
-      response.end();
-      response.redirect(`/?=${title}`);
-      
-    });
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function(error) {
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err) {
+    response.redirect(`/?=${title}`);
     });
   });
 });
@@ -140,19 +130,15 @@ app.post('/update_process', function(request, response) {
 
 // 글 삭제 처리(delete process)
 app.post('/delete_process', function(request, response) {
-  var body = '';
-  request.on('data', function(data) {
-    body = body + data;
-  });
-  request.on('end', function() {
-    var post = qs.parse(body);
-    var id = post.id;
-    var filteredId = path.parse(id).base;
-    fs.unlink(`data/${filteredId}`, function(error) {
-      response.redirect('/');
-    });
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function(error) {
+    response.redirect('/');
   });
 });
+
+
 
 
 
